3056a6589828a5a8820ebf2971cd934b3ee94c2c
sunniraleigh <sraleigh@smith.edu>
2023-03-24 14:24:29 -0400

    mess with select and slider inputs

--- app.R
+++ app.R
@@ -2,22 +2,62 @@
 library(shiny)
 library(tidyverse)
 library(leaflet)
-library(shinydashboard)
 
 # Load data
-stations <- read.csv(here::here("station-data.csv"))
+stations_data <- read.csv(here::here("station-data.csv")) %>%
+  mutate(
+    NAME = str_extract(str_to_title(NAME), "^[^,]*"),
+    DATE = as.Date(DATE, format = "%Y-%m-%d")
+    )
 
 # Define UI for application that draws a histogram
-ui <- dashboardPage(
-    leafletOutput("stations")
+ui <- fluidPage(
+  
+  selectInput("station_name", "Station Name", c("All", unique(stations_data$NAME))),
+  sliderInput("date", "Date", 
+              min = min(stations_data$DATE),
+              max = max(stations_data$DATE),
+              value = c(median(unique(stations_data$DATE)), max(unique(stations_data$DATE)))
+              ),
+  
+  tableOutput("stations")
+  # verbatimTextOutput("range")
+  # tableOutput("slider_stations")
+  
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$stations <- renderLeaflet({
-            leaflet() %>%
-            addTiles()
-    })
+  
+  # Filtering data by station name
+  # Changes the data being displayed but also changes the slider range
+  new_data <- reactive({
+    stations_data %>%
+      filter(if(input$station_name != "All") (NAME == input$station_name) else TRUE)
+  })
+  
+  # output$range <- renderPrint({ typeof(input$date[2]) })
+  
+  # slider_data <- reactive(
+  #   stations_data[
+  #     stations_data$DATE >= input$date[1] &
+  #       stations_data$DATE <= input$date[2],
+  #   ] %>%
+  #     mutate(DATE = as.character(DATE))
+  # )
+  
+  
+  # 
+  # observeEvent(new_data(), {
+  #   updateSliderInput(inputId = "date", 
+  #                     min = min(unique(new_data()$DATE)), 
+  #                     max = max(unique(new_data()$DATE))
+  #                     )
+  # })
+  # 
+  output$stations <- renderTable(head(new_data()))
+  
+  # output$slider_stations <- renderTable(head(slider_data()))
 }
 
 # Run the application 
